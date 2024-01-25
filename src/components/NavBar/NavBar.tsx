6cbe6c942316553f476337eb9a7a2ba33b407b19
prashantcoding <soultaker399@gmail.com>
2024-01-25 13:37:25 +0530

    working on cart

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,8 +3,11 @@ import MaxWidthWrapper from "../MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "../ui/button";
+import Cart from "../Cart/Cart";
 
 const NavBar = () => {
+  const user = null;
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <MaxWidthWrapper>
@@ -17,6 +20,51 @@ const NavBar = () => {
             </div>
             <div className="hidden z-50 lg:ml-8 lg:self-stretch lg:block">
               <NavItems />
+            </div>
+
+            <div className="ml-auto flex item-center">
+              <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                {user ? null : (
+                  <Link
+                    href="/sign-in"
+                    className={buttonVariants({ variant: "ghost" })}
+                  >
+                    Sign In
+                  </Link>
+                )}
+                {user ? null : (
+                  <span className="h-6 w-px bg-gray-200" arai-hidden="true" />
+                )}
+                {user ? (
+                  <p></p>
+                ) : (
+                  <Link
+                    href="/sing-up"
+                    className={buttonVariants({ variant: "ghost" })}
+                  >
+                    Create Account
+                  </Link>
+                )}
+                {user ? (
+                  <span
+                    className="h-6 w-px bg-gray-200"
+                    arai-hidden="true"
+                  ></span>
+                ) : null}
+
+                {user ? null : (
+                  <div className="flex lg:ml-6">
+                    <span
+                      className="h-6 w-px bg-gray-200"
+                      arai-hidden="true"
+                    ></span>
+                  </div>
+                )}
+
+                <div className="m-4 flow-root lg:ml-6 ">
+                  <Cart />
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/NavBar/NavItems.tsx
+++ src/components/NavBar/NavItems.tsx
@@ -1,10 +1,64 @@
 "use client";
-import React, { useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/app/config";
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "../ui/button";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
 
 const NavItems = () => {
-  const [active, setactive] = useState<null | number>(null);
+  const [activeIndex, setactiveIndex] = useState<null | number>(null);
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => {
+    setactiveIndex(null);
+  });
 
-  return <div>NavItems</div>;
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key == "Escape") {
+        setactiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", (e) => {
+      handler(e);
+    });
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setactiveIndex(null);
+          } else {
+            setactiveIndex(i);
+          }
+        };
+        const isOpen = i == activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
